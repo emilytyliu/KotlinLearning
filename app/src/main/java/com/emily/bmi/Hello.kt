9ae0c2000fcd8d7c7a782e4a2243708778d0c23c
Emily_Liu <yykliu@gmail.com>
2025-09-03 20:26:38 +0800

    Initial commit

---
+++ app/src/main/java/com/emily/bmi/Hello.kt
@@ -0,0 +1,25 @@
+package com.emily.bmi
+
+class Hello {
+}
+
+fun main() {
+    println("Hello World!")
+    //Byte, Short, Int, Long
+    val age = 20
+    val population:Long = 9999
+    //val population = 9999L
+    println(age.inc())
+    val name = "Hank"
+    println(name.get(1))
+    var name_2 = "Hank"
+    name_2 = "Jack"
+    println(name_2)
+    //Float(32), Double(64)
+    val weight = 65.5f
+    //true, false
+    val isAdult = true
+    //Char
+    val c = 'A'
+    println(c)
+}
