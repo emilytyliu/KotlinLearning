ce6b89c603350e9c25673c9020feecf55d153ea0
Emily_Liu <yykliu@gmail.com>
2025-09-08 18:34:46 +0800

    Student 類別的設計方法 C3U6

--- app/src/main/java/com/emily/bmi/student/Student.kt
+++ app/src/main/java/com/emily/bmi/student/Student.kt
@@ -4,19 +4,67 @@ package com.emily.bmi.student
 //印出學生平均成績
 //印出學生等級(A,B,C,D,F)
 
-class student(val id: String, val name: String, var english:Int, var math:Int) {
-    constructor(id: String, name: String) : this(id, name, 0, 0)
+class Student(val id: String, val name: String, var english: Int = 0, var math: Int = 0) {
+    //constructor(id: String, name: String) : this(id, name, 0, 0)
     fun print() {
-        println("$id\t$name\t$english\t$math")
+        println("$id\t$name\t$english\t$math\t${average()}\t${average01()}\t${grading()}\t${grading01()}\t${grading02()}")
+    }
+
+    fun average(): Int {
+        return (english + math) / 2
+    }
+
+    fun average01() = (english + math) / 2
+    fun grading(): String {
+        val grade = when (average() / 10) {
+            9, 10 -> "A"
+            8 -> "B"
+            7 -> "C"
+            6 -> "D"
+            else -> "F"
+        }
+        return grade
+    }
+
+    fun grading01(): String {
+        return when (average() / 10) {
+            9, 10 -> "A"
+            8 -> "B"
+            7 -> "C"
+            6 -> "D"
+            else -> "F"
+        }
+    }
+
+    fun grading02() = when (average() / 10) {
+        9, 10 -> "A"
+        8 -> "B"
+        7 -> "C"
+        6 -> "D"
+        else -> "F"
     }
 }
 
 fun main() {
-    val stu1 = student("001", "Jack", 70,80)
-    val stu2 = student("002", "Hank", 66,87)
-    val stu3 = student("003","Jane")
+
+    val stu1 = Student("001", "Jack", 95, 99)
+    val stu2 = Student("002", "Hank", 66, 87)
+    val stu3 = Student("003", "Jane")
     stu1.print()
     stu2.print()
     stu3.print()
+    println()
 
+    val students = listOf<Student>(
+        Student("001", "Jack", 95, 99),
+        Student("002", "Hank", 66, 87),
+        Student("003", "Jane")
+    )
+    for (i in 0..2) {
+        students.get(i).print()
+    }
+    println()
+    for (i in students) {
+        i.print()
+    }
 }
