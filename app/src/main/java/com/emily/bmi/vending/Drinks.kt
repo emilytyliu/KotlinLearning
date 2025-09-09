84f7e221be051510b4303d55fd7b83448926c4c1
Emily_Liu <yykliu@gmail.com>
2025-09-09 15:12:02 +0800

    Drinks+Drink C4U3 data class
    新增一個類別，預設繼承物件 java.lang.Object
    在一個類別裡要覆寫父類別的方法：Ctrl+O
    data class
    可以直接print
    可以.copy

--- app/src/main/java/com/emily/bmi/vending/Drinks.kt
+++ app/src/main/java/com/emily/bmi/vending/Drinks.kt
@@ -1,6 +1,11 @@
-package com.emily.bmi
+package com.emily.bmi.vending
 
 fun main() {
+    val drink1 = Drink("tea", 0, 50)
+    println(drink1)
+    val drink2 = drink1.copy(suger = 5)
+    println(drink2)
+
     val drinks = mutableMapOf<Int, String>(
         1 to "Coke",
         2 to "7-up",
