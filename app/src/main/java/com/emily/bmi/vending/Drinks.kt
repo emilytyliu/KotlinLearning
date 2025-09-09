d30b096158bb08951ef616804742020f09e416cf
Emily_Liu <yykliu@gmail.com>
2025-09-09 15:37:44 +0800

    Drinks+Drink C4U4 Extensions
    功能的擴充
    特別可以用在不知道父類別的原始碼
    寫一個fun XX.yyy
    放在main方法的同個階層就可以

--- app/src/main/java/com/emily/bmi/vending/Drinks.kt
+++ app/src/main/java/com/emily/bmi/vending/Drinks.kt
@@ -1,10 +1,6 @@
 package com.emily.bmi.vending
 
 fun main() {
-    val drink1 = Drink("tea", 0, 50)
-    println(drink1)
-    val drink2 = drink1.copy(suger = 5)
-    println(drink2)
 
     val drinks = mutableMapOf<Int, String>(
         1 to "Coke",
@@ -16,4 +12,28 @@ fun main() {
     println(drinks.get(3))
     drinks.put(5, "Water")
     println(drinks)
+
+    val drink1 = Drink("tea", 0, 50)
+    println(drink1)
+    val drink2 = drink1.copy(suger = 5)
+    println(drink2)
+
+    println()
+    println("C4U4 Extension")
+    val s="abcdef"
+    println(s.validate())
+
+    println()
+    val discountDrink2 = drink2.off20()
+    println(drink2)
+    println(discountDrink2)
+
+}
+
+fun String.validate(): Boolean {
+    return this.length >=6
+}
+
+fun Drink.off20():Drink {
+    return this.copy(price = (this.price *0.8).toInt())
 }
