c4dd1b84abfc238c0852bbf8d91d66df1b70c0c1
Emily_Liu <yykliu@gmail.com>
2025-09-10 18:32:52 +0800

    C5U3 MainActivity2 R. 是自動產生的 好用的icon資源：flaticon.com 定義res string color 搜尋 Android Jetpack

--- app/src/main/java/com/emily/bmi/MainActivity2.kt
+++ app/src/main/java/com/emily/bmi/MainActivity2.kt
@@ -11,10 +11,14 @@ class MainActivity2 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        //R.id.hello_main
+        //R.drawable.pig
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
+
         }
     }
 }
